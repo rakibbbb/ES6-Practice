8be865424b770468cd491f6c1ba87967b2f77c82
Md. Rakibuzzaman <rakib141746@gmail.com>
2021-02-02 01:34:26 +0600

    js templete

---
+++ template.js
@@ -0,0 +1,13 @@
+const firstName = "Bill";
+const lastName = "Gates";
+const fullName = firstName + ' ' + lastName;
+const fullName1 = `${firstName} ${lastName} is the richest person in the world.`
+console.log(fullName);
+console.log(fullName1);
+
+const multiline = `I love you
+I miss you
+no, i seriously love you
+please don't go.`
+
+console.log(multiline);
