6f7d74324e44ecf7c27b6af2e98140c9715ea834
Md. Rakibuzzaman <rakib141746@gmail.com>
2021-02-02 01:14:47 +0600

    let & const

---
+++ letconst.js
@@ -0,0 +1,16 @@
+const id = 4555;
+//id = 3333; Const type variable can't be change
+console.log(id);
+
+const array = ['rakib', 'shamim', 'nishy', 'ritu'];
+array.push('bristy');
+array[1] = 'nila';
+console.log(array);
+
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+}
+console.log(sum);
+console.log(i);
